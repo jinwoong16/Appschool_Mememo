d765c21b6445d20c7833941dfd8d526bde05d17b
jinwoong <baseball0020@gmail.com>
2024-04-22 15:04:45 +0900

    chore: first init

---
+++ Mememo/MememoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MememoApp.swift
+//  Mememo
+//
+//  Created by jinwoong Kim on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MememoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
